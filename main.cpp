b79760ea11d996c51d0036cbab2a8b94702c7de7
RayPlusC <ray1037@gmail.com>
2024-05-28 13:27:30 +0200

    Minimalised
    Started as demo with the grains
    Added rest of code, but black screen

--- main.cpp
+++ main.cpp
@@ -45,276 +45,59 @@ struct mesh {
 	}
 };
 
-struct mat4x4{
-	float m[4][4] = { 0 };
-};
-
-class Quarto : public olc::PixelGameEngine
+class Example : public olc::PixelGameEngine
 {
 public:
-	Quarto()
+	Example()
 	{
-		sAppName = "Quarto!";
-	}
-private:
-	mesh q0000;
-	mesh q0001;
-	mesh q0010;
-	mesh q0011;
-	mesh q0100;
-	mesh q0101;
-	mesh q0110;
-	mesh q0111;
-	mesh q1000;
-	mesh q1001;
-	mesh q1010;
-	mesh q1011;
-	mesh q1100;
-	mesh q1101;
-	mesh q1110;
-	mesh q1111;
-	mesh meshBoard;
-	mesh meshCube;
-
-	mat4x4 matProj;
-
-	float fYaw;		// FPS Camera rotation in XZ plane
-	float fTheta;
-
-	olc::GFX3D::vec3d vCamera;
-
-	float fCameraX = 0.0f;
-	float fCameraY = 0.0f;
-	float fCameraZ = 0.0f;
-
-	int nMouseWorldX = 0;
-	int nMouseWorldY = 0;
-	int nOldMouseWorldX = 0;
-	int nOldMouseWorldY = 0;
-
-	bool bMouseDown = false;
-	
-
-	olc::GFX3D::PipeLine pipeRender;
-	//olc::GFX3D::mat4x4 matProj;
-	olc::GFX3D::vec3d vUp = { 0,1,0 };
-	olc::GFX3D::vec3d vEye = { 0,0,1 };
-	olc::GFX3D::vec3d vLookDir = { 0,0,1 };
-	olc::GFX3D::vec3d vTarget = Vector_Add(vCamera, vLookDir);
-
-	mat4x4 matCamera = Matrix_PointAt(vCamera, vTarget, vUp);
-
-	mat4x4 matView = Matrix_QuickInverse(matCamera);
-
-	olc::GFX3D::vec3d viewWorldTopLeft, viewWorldBottomRight;
-
-	olc::GFX3D::vec3d Matrix_MultiplyVector(mat4x4& m, olc::GFX3D::vec3d& i)
-	{
-		olc::GFX3D::vec3d v;
-		v.x = i.x * m.m[0][0] + i.y * m.m[1][0] + i.z * m.m[2][0] + i.w * m.m[3][0];
-		v.y = i.x * m.m[0][1] + i.y * m.m[1][1] + i.z * m.m[2][1] + i.w * m.m[3][1];
-		v.z = i.x * m.m[0][2] + i.y * m.m[1][2] + i.z * m.m[2][2] + i.w * m.m[3][2];
-		v.w = i.x * m.m[0][3] + i.y * m.m[1][3] + i.z * m.m[2][3] + i.w * m.m[3][3];
-		return v;
-	}
-
-	mat4x4 Matrix_MakeIdentity()
-	{
-		mat4x4 matrix;
-		matrix.m[0][0] = 1.0f;
-		matrix.m[1][1] = 1.0f;
-		matrix.m[2][2] = 1.0f;
-		matrix.m[3][3] = 1.0f;
-		return matrix;
-	}
-
-	mat4x4 Matrix_MakeRotationX(float fAngleRad)
-	{
-		mat4x4 matrix;
-		matrix.m[0][0] = 1.0f;
-		matrix.m[1][1] = cosf(fAngleRad);
-		matrix.m[1][2] = sinf(fAngleRad);
-		matrix.m[2][1] = -sinf(fAngleRad);
-		matrix.m[2][2] = cosf(fAngleRad);
-		matrix.m[3][3] = 1.0f;
-		return matrix;
-	}
-
-	mat4x4 Matrix_MakeRotationY(float fAngleRad)
-	{
-		mat4x4 matrix;
-		matrix.m[0][0] = cosf(fAngleRad);
-		matrix.m[0][2] = sinf(fAngleRad);
-		matrix.m[2][0] = -sinf(fAngleRad);
-		matrix.m[1][1] = 1.0f;
-		matrix.m[2][2] = cosf(fAngleRad);
-		matrix.m[3][3] = 1.0f;
-		return matrix;
-	}
-
-	mat4x4 Matrix_MakeRotationZ(float fAngleRad)
-	{
-		mat4x4 matrix;
-		matrix.m[0][0] = cosf(fAngleRad);
-		matrix.m[0][1] = sinf(fAngleRad);
-		matrix.m[1][0] = -sinf(fAngleRad);
-		matrix.m[1][1] = cosf(fAngleRad);
-		matrix.m[2][2] = 1.0f;
-		matrix.m[3][3] = 1.0f;
-		return matrix;
-	}
-
-	mat4x4 Matrix_MakeTranslation(float x, float y, float z)
-	{
-		mat4x4 matrix;
-		matrix.m[0][0] = 1.0f;
-		matrix.m[1][1] = 1.0f;
-		matrix.m[2][2] = 1.0f;
-		matrix.m[3][3] = 1.0f;
-		matrix.m[3][0] = x;
-		matrix.m[3][1] = y;
-		matrix.m[3][2] = z;
-		return matrix;
-	}
-
-	mat4x4 Matrix_MakeProjection(float fFovDegrees, float fAspectRatio, float fNear, float fFar)
-	{
-		float fFovRad = 1.0f / tanf(fFovDegrees * 0.5f / 180.0f * 3.14159f);
-		mat4x4 matrix;
-		matrix.m[0][0] = fAspectRatio * fFovRad;
-		matrix.m[1][1] = fFovRad;
-		matrix.m[2][2] = fFar / (fFar - fNear);
-		matrix.m[3][2] = (-fFar * fNear) / (fFar - fNear);
-		matrix.m[2][3] = 1.0f;
-		matrix.m[3][3] = 0.0f;
-		return matrix;
-	}
-
-	mat4x4 Matrix_MultiplyMatrix(mat4x4& m1, mat4x4& m2)
-	{
-		mat4x4 matrix;
-		for (int c = 0; c < 4; c++)
-			for (int r = 0; r < 4; r++)
-				matrix.m[r][c] = m1.m[r][0] * m2.m[0][c] + m1.m[r][1] * m2.m[1][c] + m1.m[r][2] * m2.m[2][c] + m1.m[r][3] * m2.m[3][c];
-		return matrix;
-	}
-
-	mat4x4 Matrix_PointAt(olc::GFX3D::vec3d& pos, olc::GFX3D::vec3d& target, olc::GFX3D::vec3d& up)
-	{
-		// Calculate new forward direction
-		olc::GFX3D::vec3d newForward = Vector_Sub(target, pos);
-		newForward = Vector_Normalise(newForward);
-
-		// Calculate new Up direction
-		olc::GFX3D::vec3d a = Vector_Mul(newForward, Vector_DotProduct(up, newForward));
-		olc::GFX3D::vec3d newUp = Vector_Sub(up, a);
-		newUp = Vector_Normalise(newUp);
-
-		// New Right direction is easy, its just cross product
-		olc::GFX3D::vec3d newRight = Vector_CrossProduct(newUp, newForward);
-
-		// Construct Dimensioning and Translation Matrix	
-		mat4x4 matrix;
-		matrix.m[0][0] = newRight.x;	matrix.m[0][1] = newRight.y;	matrix.m[0][2] = newRight.z;	matrix.m[0][3] = 0.0f;
-		matrix.m[1][0] = newUp.x;		matrix.m[1][1] = newUp.y;		matrix.m[1][2] = newUp.z;		matrix.m[1][3] = 0.0f;
-		matrix.m[2][0] = newForward.x;	matrix.m[2][1] = newForward.y;	matrix.m[2][2] = newForward.z;	matrix.m[2][3] = 0.0f;
-		matrix.m[3][0] = pos.x;			matrix.m[3][1] = pos.y;			matrix.m[3][2] = pos.z;			matrix.m[3][3] = 1.0f;
-		return matrix;
-
-	}
-
-	mat4x4 Matrix_QuickInverse(mat4x4& m) // Only for Rotation/Translation Matrices
-	{
-		mat4x4 matrix;
-		matrix.m[0][0] = m.m[0][0]; matrix.m[0][1] = m.m[1][0]; matrix.m[0][2] = m.m[2][0]; matrix.m[0][3] = 0.0f;
-		matrix.m[1][0] = m.m[0][1]; matrix.m[1][1] = m.m[1][1]; matrix.m[1][2] = m.m[2][1]; matrix.m[1][3] = 0.0f;
-		matrix.m[2][0] = m.m[0][2]; matrix.m[2][1] = m.m[1][2]; matrix.m[2][2] = m.m[2][2]; matrix.m[2][3] = 0.0f;
-		matrix.m[3][0] = -(m.m[3][0] * matrix.m[0][0] + m.m[3][1] * matrix.m[1][0] + m.m[3][2] * matrix.m[2][0]);
-		matrix.m[3][1] = -(m.m[3][0] * matrix.m[0][1] + m.m[3][1] * matrix.m[1][1] + m.m[3][2] * matrix.m[2][1]);
-		matrix.m[3][2] = -(m.m[3][0] * matrix.m[0][2] + m.m[3][1] * matrix.m[1][2] + m.m[3][2] * matrix.m[2][2]);
-		matrix.m[3][3] = 1.0f;
-		return matrix;
-	}
-
-	olc::GFX3D::vec3d Vector_Add(olc::GFX3D::vec3d& v1, olc::GFX3D::vec3d& v2)
-	{
-		return { v1.x + v2.x, v1.y + v2.y, v1.z + v2.z };
-	}
-
-	olc::GFX3D::vec3d Vector_Sub(olc::GFX3D::vec3d& v1, olc::GFX3D::vec3d& v2)
-	{
-		return { v1.x - v2.x, v1.y - v2.y, v1.z - v2.z };
-	}
-
-	olc::GFX3D::vec3d Vector_Mul(olc::GFX3D::vec3d& v1, float k)
-	{
-		return { v1.x * k, v1.y * k, v1.z * k };
-	}
-
-	olc::GFX3D::vec3d Vector_Div(olc::GFX3D::vec3d& v1, float k)
-	{
-		return { v1.x / k, v1.y / k, v1.z / k };
-	}
-
-	float Vector_DotProduct(olc::GFX3D::vec3d& v1, olc::GFX3D::vec3d& v2)
-	{
-		return v1.x*v2.x +  v1.y*v2.y, v1.z*v2.z;
-	}
-
-	float Vector_Length(olc::GFX3D::vec3d& v)
-	{
-		return sqrtf(Vector_DotProduct(v, v));
-	}
-
-	olc::GFX3D::vec3d Vector_Normalise(olc::GFX3D::vec3d& v)
-	{
-		float l = Vector_Length(v);
-		return { v.x / l, v.y / l, v.z / l };
-	}
-
-	olc::GFX3D::vec3d Vector_CrossProduct(olc::GFX3D::vec3d& v1, olc::GFX3D::vec3d& v2)
-	{
-		olc::GFX3D::vec3d v;
-		v.x = v1.y * v2.z - v1.z * v2.y;
-		v.y = v1.z * v2.x - v1.x * v2.z;
-		v.z = v1.x * v2.y - v1.y * v2.x;
-		return v;
+		// Name your application
+		sAppName = "Example";
 	}
 
 public:
 	olc::Sprite* spr_logo = nullptr;
 	olc::Decal* dec_logo = nullptr;
+	mesh meshBoard, meshCube;
+	olc::GFX3D::mat4x4 matProj;
+	olc::GFX3D::PipeLine pipeRender;
+	//olc::GFX3D::mat4x4 matProj;
+	float fYaw;		// FPS Camera rotation in XZ plane
+	float fTheta;
+	olc::GFX3D::vec3d vCamera;
+	olc::GFX3D::vec3d vUp = { 0,1,0 };
+	olc::GFX3D::vec3d vEye = { 0,0,1 };
+	olc::GFX3D::vec3d vLookDir = { 0,0,1 };
+	float fCameraX = 0.0f;
+	float fCameraY = 0.0f;
+	float fCameraZ = 0.0f;
+	olc::GFX3D::vec3d vTarget = olc::GFX3D::Math::Vec_Add(vCamera, vLookDir);
 
 	bool OnUserCreate() override
 	{
 		spr_logo = new olc::Sprite("pge2_logo.png");
 		dec_logo = new olc::Decal(spr_logo);
 		// Called once at the start, so create things here
-		
+
 
 		//q1111.LoadFromObjectFile("1111.obj");
 		//q1110.LoadFromObjectFile("1110.obj");
 		//q1101.LoadFromObjectFile("1101.obj");
-		meshBoard.LoadFromObjectFile("board.obj");
-		//meshCube.LoadFromObjectFile("teapot.obj");
+		//meshBoard.LoadFromObjectFile("board.obj");
+		meshCube.LoadFromObjectFile("teapot.obj");
 
 
-		matProj = Matrix_MakeProjection(90.0f, (float)ScreenHeight() / (float)ScreenWidth(), 0.1f, 1000.0f);
+		matProj = olc::GFX3D::Math::Mat_MakeProjection(90.0f, (float)ScreenHeight() / (float)ScreenWidth(), 0.1f, 1000.0f);
 		return true;
 	}
-	
+	//bool OnUserUpdate() over
+
 	bool OnUserUpdate(float fElapsedTime) override
 	{
-		// called once per frame
-		Clear(olc::DARK_BLUE);		
-		/*for (int x = 0; x < ScreenWidth(); x++)
-			for (int y = 0; y < ScreenHeight(); y++)
-				Draw(x, y, olc::Pixel(olc::DARK_BLUE));
-		*/
-		
-		DrawDecal({ 0.0f, 0.0f }, dec_logo, { 0.3f, 0.3f });
+		//Clear(olc::DARK_BLUE);
+
+		// Called once per frame, draws random coloured pixels
 		vEye = { fCameraX,fCameraY,fCameraZ };
-		olc::GFX3D::vec3d vLookTarget = Vector_Add(vEye, vLookDir);
+		olc::GFX3D::vec3d vLookTarget = olc::GFX3D::Math::Vec_Add(vEye, vLookDir);
 
 		// Setup the camera properties for the pipeline - aka "view" transform
 		pipeRender.SetCamera(vEye, vLookTarget, vUp);
@@ -332,155 +115,91 @@ public:
 
 		if (GetKey(olc::Key::RIGHT).bHeld)
 			vCamera.x += 8.0f * fElapsedTime;	// Travel Along X-Axis
-		///////
-		
 
-		olc::GFX3D::vec3d vForward = Vector_Mul(vLookDir, 8.0f * fElapsedTime);
+		olc::GFX3D::vec3d vForward = olc::GFX3D::Math::Vec_Mul(vLookDir, 8.0f * fElapsedTime);
 
 		// Set up "World Tranmsform" though not updating theta 
 		// makes this a bit redundant
-		mat4x4 matRotZ, matRotX;
+		olc::GFX3D::mat4x4 matRotZ, matRotX;
 		//fTheta += 1.0f * fElapsedTime; // Uncomment to spin me right round baby right round
-		matRotZ = Matrix_MakeRotationZ(fTheta * 0.5f);
-		matRotX = Matrix_MakeRotationX(fTheta);
+		matRotZ = olc::GFX3D::Math::Mat_MakeRotationZ(fTheta * 0.5f);
+		matRotX = olc::GFX3D::Math::Mat_MakeRotationX(fTheta);
 
-		mat4x4 matTrans;
-		matTrans = Matrix_MakeTranslation(0.0f, 0.0f, 5.0f);
+		olc::GFX3D::mat4x4 matTrans;
+		matTrans = olc::GFX3D::Math::Mat_MakeTranslation(0.0f, 0.0f, 5.0f);
 
-		mat4x4 matWorld;
-		matWorld = Matrix_MakeIdentity();	// Form World Matrix
-		matWorld = Matrix_MultiplyMatrix(matRotZ, matRotX); // Transform by rotation
-		matWorld = Matrix_MultiplyMatrix(matWorld, matTrans); // Transform by translation
-		
+		olc::GFX3D::mat4x4 matWorld;
+		matWorld = olc::GFX3D::Math::Mat_MakeIdentity();	// Form World Matrix
+		matWorld = olc::GFX3D::Math::Mat_MultiplyMatrix(matRotZ, matRotX); // Transform by rotation
+		matWorld = olc::GFX3D::Math::Mat_MultiplyMatrix(matWorld, matTrans); // Transform by translation
+
 		// Create "Point At" Matrix for camera
 		olc::GFX3D::vec3d vUp = { 0,1,0 };
 		olc::GFX3D::vec3d vTarget = { 0,0,1 };
-		mat4x4 matCameraRot = Matrix_MakeRotationY(fYaw);
-		
-		vLookDir = Matrix_MultiplyVector(matCameraRot, vTarget);
-		
-		vTarget = Vector_Add(vCamera, vLookDir);
-		mat4x4 matCamera = Matrix_PointAt(vCamera, vTarget, vUp);
+		olc::GFX3D::mat4x4 matCameraRot = olc::GFX3D::Math::Mat_MakeRotationY(fYaw);
+
+		vLookDir = olc::GFX3D::Math::Mat_MultiplyVector(matCameraRot, vTarget);
+
+		vTarget = olc::GFX3D::Math::Vec_Add(vCamera, vLookDir);
+		olc::GFX3D::mat4x4 matCamera = olc::GFX3D::Math::Mat_PointAt(vCamera, vTarget, vUp);
 
 		// Make view matrix from camera
-		mat4x4 matView = Matrix_QuickInverse(matCamera);
+		olc::GFX3D::mat4x4 matView = olc::GFX3D::Math::Mat_QuickInverse(matCamera);
 
 		// Store triagles for rastering later
 		std::vector<olc::GFX3D::triangle> vecTrianglesToRaster;
 
 		// Draw Triangles
-		for (auto tri : meshBoard.tris)
-		{
-			olc::GFX3D::triangle triProjected, triTransformed, triViewed;
-			
-			triTransformed.p[0] = Matrix_MultiplyVector(matWorld, tri.p[0]);
-			triTransformed.p[1] = Matrix_MultiplyVector(matWorld, tri.p[1]);
-			triTransformed.p[2] = Matrix_MultiplyVector(matWorld, tri.p[2]);
-			
-			//Calculate triangle Normal
-			olc::GFX3D::vec3d normal, line1, line2;
-
-			//gets lines either side of triange
-			line1 = Vector_Sub(triTransformed.p[1], triTransformed.p[0]);
-			line2 = Vector_Sub(triTransformed.p[1], triTransformed.p[0]);
-
-			// Take cross product ofl ines to get normal to triangle surface
-			normal = Vector_CrossProduct(line1, line2);
-
-			// You normally need to normalise a normal!
-			normal = Vector_Normalise(normal);
-
-			// Get ray from triangle to camera
-			olc::GFX3D::vec3d vCameraRay = Vector_Sub(triTransformed.p[0], vCamera);
-
-			// If ray is aligned with normal, then triangle is visible
-			if (Vector_DotProduct(normal, vCameraRay) < 0.0f)
-			{
-				// Illumination
-				olc::GFX3D::vec3d light_direction = { 0.0f, 1.0f, 1.0f };
-				light_direction = Vector_Normalise(light_direction);
-
-				float dp = std::max(0.1f, Vector_DotProduct(light_direction, normal));
-
-				// Project triangles from 3D --> 2D
-				triProjected.p[0] = Matrix_MultiplyVector(matProj, triTransformed.p[0]);
-				triProjected.p[1] = Matrix_MultiplyVector(matProj, triTransformed.p[1]);
-				triProjected.p[2] = Matrix_MultiplyVector(matProj, triTransformed.p[2]);
-
-				//Normalise manualy
-				triProjected.p[0] = Vector_Div(triProjected.p[0], triProjected.p[0].w);
-				triProjected.p[1] = Vector_Div(triProjected.p[1], triProjected.p[1].w);
-				triProjected.p[2] = Vector_Div(triProjected.p[2], triProjected.p[2].w);
-
-
-				// Scale into view
-				olc::GFX3D::vec3d vOffsetView = { 1,1,0 };
-				triProjected.p[0] = Vector_Add(triProjected.p[0], vOffsetView);
-				triProjected.p[1] = Vector_Add(triProjected.p[1], vOffsetView);
-				triProjected.p[2] = Vector_Add(triProjected.p[2], vOffsetView);
-				triProjected.p[0].x *= 0.5f * (float)ScreenWidth();
-				triProjected.p[0].y *= 0.5f * (float)ScreenHeight();
-				triProjected.p[1].x *= 0.5f * (float)ScreenWidth();
-				triProjected.p[1].y *= 0.5f * (float)ScreenHeight();
-				triProjected.p[2].x *= 0.5f * (float)ScreenWidth();
-				triProjected.p[2].y *= 0.5f * (float)ScreenHeight();
-
-				// Store triangle for sorting
-				vecTrianglesToRaster.push_back(triProjected);
-			}
-			
-		}
-
-		// Draw Triangles
 		for (auto tri : meshCube.tris)
 		{
+			DrawRectDecal({20,20}, { 20,20 });
 			olc::GFX3D::triangle triProjected, triTransformed, triViewed;
 
-			triTransformed.p[0] = Matrix_MultiplyVector(matWorld, tri.p[0]);
-			triTransformed.p[1] = Matrix_MultiplyVector(matWorld, tri.p[1]);
-			triTransformed.p[2] = Matrix_MultiplyVector(matWorld, tri.p[2]);
+			triTransformed.p[0] = olc::GFX3D::Math::Mat_MultiplyVector(matWorld, tri.p[0]);
+			triTransformed.p[1] = olc::GFX3D::Math::Mat_MultiplyVector(matWorld, tri.p[1]);
+			triTransformed.p[2] = olc::GFX3D::Math::Mat_MultiplyVector(matWorld, tri.p[2]);
 
 			//Calculate triangle Normal
 			olc::GFX3D::vec3d normal, line1, line2;
 
 			//gets lines either side of triange
-			line1 = Vector_Sub(triTransformed.p[1], triTransformed.p[0]);
-			line2 = Vector_Sub(triTransformed.p[1], triTransformed.p[0]);
+			line1 = olc::GFX3D::Math::Vec_Sub(triTransformed.p[1], triTransformed.p[0]);
+			line2 = olc::GFX3D::Math::Vec_Sub(triTransformed.p[1], triTransformed.p[0]);
 
 			// Take cross product ofl ines to get normal to triangle surface
-			normal = Vector_CrossProduct(line1, line2);
+			normal = olc::GFX3D::Math::Vec_CrossProduct(line1, line2);
 
 			// You normally need to normalise a normal!
-			normal = Vector_Normalise(normal);
+			normal = olc::GFX3D::Math::Vec_Normalise(normal);
 
 			// Get ray from triangle to camera
-			olc::GFX3D::vec3d vCameraRay = Vector_Sub(triTransformed.p[0], vCamera);
+			olc::GFX3D::vec3d vCameraRay = olc::GFX3D::Math::Vec_Sub(triTransformed.p[0], vCamera);
 
 			// If ray is aligned with normal, then triangle is visible
-			if (Vector_DotProduct(normal, vCameraRay) < 0.0f)
+			if (olc::GFX3D::Math::Vec_DotProduct(normal, vCameraRay) < 0.0f)
 			{
 				// Illumination
 				olc::GFX3D::vec3d light_direction = { 0.0f, 1.0f, 1.0f };
-				light_direction = Vector_Normalise(light_direction);
+				light_direction = olc::GFX3D::Math::Vec_Normalise(light_direction);
 
-				float dp = std::max(0.1f, Vector_DotProduct(light_direction, normal));
+				float dp = std::max(0.1f, olc::GFX3D::Math::Vec_DotProduct(light_direction, normal));
 
 				// Project triangles from 3D --> 2D
-				triProjected.p[0] = Matrix_MultiplyVector(matProj, triTransformed.p[0]);
-				triProjected.p[1] = Matrix_MultiplyVector(matProj, triTransformed.p[1]);
-				triProjected.p[2] = Matrix_MultiplyVector(matProj, triTransformed.p[2]);
+				triProjected.p[0] = olc::GFX3D::Math::Mat_MultiplyVector(matProj, triTransformed.p[0]);
+				triProjected.p[1] = olc::GFX3D::Math::Mat_MultiplyVector(matProj, triTransformed.p[1]);
+				triProjected.p[2] = olc::GFX3D::Math::Mat_MultiplyVector(matProj, triTransformed.p[2]);
 
 				//Normalise manualy
-				triProjected.p[0] = Vector_Div(triProjected.p[0], triProjected.p[0].w);
-				triProjected.p[1] = Vector_Div(triProjected.p[1], triProjected.p[1].w);
-				triProjected.p[2] = Vector_Div(triProjected.p[2], triProjected.p[2].w);
+				triProjected.p[0] = olc::GFX3D::Math::Vec_Div(triProjected.p[0], triProjected.p[0].w);
+				triProjected.p[1] = olc::GFX3D::Math::Vec_Div(triProjected.p[1], triProjected.p[1].w);
+				triProjected.p[2] = olc::GFX3D::Math::Vec_Div(triProjected.p[2], triProjected.p[2].w);
 
 
 				// Scale into view
 				olc::GFX3D::vec3d vOffsetView = { 1,1,0 };
-				triProjected.p[0] = Vector_Add(triProjected.p[0], vOffsetView);
-				triProjected.p[1] = Vector_Add(triProjected.p[1], vOffsetView);
-				triProjected.p[2] = Vector_Add(triProjected.p[2], vOffsetView);
+				triProjected.p[0] = olc::GFX3D::Math::Vec_Add(triProjected.p[0], vOffsetView);
+				triProjected.p[1] = olc::GFX3D::Math::Vec_Add(triProjected.p[1], vOffsetView);
+				triProjected.p[2] = olc::GFX3D::Math::Vec_Add(triProjected.p[2], vOffsetView);
 				triProjected.p[0].x *= 0.5f * (float)ScreenWidth();
 				triProjected.p[0].y *= 0.5f * (float)ScreenHeight();
 				triProjected.p[1].x *= 0.5f * (float)ScreenWidth();
@@ -491,44 +210,40 @@ public:
 				// Store triangle for sorting
 				vecTrianglesToRaster.push_back(triProjected);
 			}
+			// Sort triangles from back to front
+			sort(vecTrianglesToRaster.begin(), vecTrianglesToRaster.end(), [](olc::GFX3D::triangle& t1, olc::GFX3D::triangle& t2)
+				{
+					float z1 = (t1.p[0].z + t1.p[1].z + t1.p[2].z) / 3.0f;
+					float z2 = (t2.p[0].z + t2.p[1].z + t2.p[2].z) / 3.0f;
+					return z1 > z2;
+				});
 
-		}
-		// Sort triangles from back to front
-		sort(vecTrianglesToRaster.begin(), vecTrianglesToRaster.end(), [](olc::GFX3D::triangle& t1, olc::GFX3D::triangle& t2)
+			for (auto& triProjected : vecTrianglesToRaster)
 			{
-				float z1 = (t1.p[0].z + t1.p[1].z + t1.p[2].z) / 3.0f;
-				float z2 = (t2.p[0].z + t2.p[1].z + t2.p[2].z) / 3.0f;
-				return z1 > z2;
-			});
+				// Rasterize triangle
+				FillTriangle(triProjected.p[0].x, triProjected.p[0].y,
+					triProjected.p[1].x, triProjected.p[1].y,
+					triProjected.p[2].x, triProjected.p[2].y,
+					olc::Pixel(olc::BLACK));
+				// Mesh	
+				DrawTriangle(triProjected.p[0].x, triProjected.p[0].y,
+					triProjected.p[1].x, triProjected.p[1].y,
+					triProjected.p[2].x, triProjected.p[2].y,
+					olc::Pixel(olc::GREY));
+			}
 
-		for (auto& triProjected : vecTrianglesToRaster)
-		{
-			// Rasterize triangle
-			FillTriangle(triProjected.p[0].x, triProjected.p[0].y,
-				triProjected.p[1].x, triProjected.p[1].y,
-				triProjected.p[2].x, triProjected.p[2].y,
-				olc::Pixel(olc::BLACK));
-			// Mesh	
-			DrawTriangle(triProjected.p[0].x, triProjected.p[0].y,
-				triProjected.p[1].x, triProjected.p[1].y,
-				triProjected.p[2].x, triProjected.p[2].y,
-				olc::Pixel(olc::GREY));
+			// OLC logo for copyright
+			DrawDecal({ 0.0f, 0.0f }, dec_logo, { 0.25f, 0.25f });
+			
+			return true;
 		}
-
-		return true;
 	}
-	
-
-
 };
-
 
 int main()
 {
-	Quarto game;
-	if (game.Construct(1024, 768, 1, 1))
-		game.Start();
-
+	Example demo;
+	if (demo.Construct(800, 600, 1, 1))
+		demo.Start();
 	return 0;
 }
-
